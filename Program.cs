b49b242616abb0a01f92eef6e8fade350e1007d7
Wosbaldo <rm0796042022@unab.edu.sv>
2023-03-17 21:57:17 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Almacen.Models;
+
+//Console.WriteLine("Hello, World!");
+
+
+using (AlmacenContext db = new AlmacenContext())
+{
+    var ListProductos = db.Productos.ToList();
+
+    foreach (var product in ListProductos)
+    {
+        Console.WriteLine(product.Id);
+        Console.WriteLine(product.Nombre);
+        Console.WriteLine(product.Descripción);
+        Console.WriteLine(product.Precio);
+        Console.WriteLine(product.Stokck);
+    }
+
+    //Producto producto = new Producto();
+    //producto.Id = 01;
+    //producto.Nombre = "Manzana";
+    //producto.Descripción = "Frutas frescas";
+    //producto.Precio = 2;
+    //producto.Stokck = 50;
+
+    //db.Productos.Add(producto);
+    //db.SaveChanges();
+}
